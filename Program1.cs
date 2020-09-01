f6f503391d8b28ab902cd86cc413faa2ae38719c
Gurubani Gulati <gurbanigulati17@gmail.com>
2020-09-01 19:50:50 +0530

    Create Program1.cs

---
+++ Program1.cs
@@ -0,0 +1,19 @@
+using System;
+
+
+    class Program1
+    {
+ 
+
+    static void Main(string[] args)
+        {
+          //Printing Hello World
+          
+          // Console.WriteLine is used to render line of text on console.
+          
+          Console.WriteLine("Hello World");
+          
+          }
+          
+          
+       }   
