f328214a4245ab96ee328a8928afba8eed77de16
Gurubani Gulati <gurbanigulati17@gmail.com>
2020-09-04 14:44:11 +0530

    Add files via upload

---
+++ C# Programs/Program 17- Print First K characters of String/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+    class Program
+    {
+ 
+
+  
+        
+
+  
+     public  static string firstKCharacters(string str, int k) 
+       { 
+        
+      
+      if (str == null ) 
+          return null; 
+      if (str.Length > k) 
+          return str.Substring(0, k); 
+      else
+          return str; 
+  } 
+    
+  
+static void Main(string[] args) 
+  { 
+      
+      string str = "JacobScott"; 
+      int k = 5;
+      Console.WriteLine(firstKCharacters(str, k)); 
+  } 
+        
+    }
+
